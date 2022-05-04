78cd13f48e870017540c9f49369c36095eb4aa5a
Jacek Dudek <JacekZbigDudek@hotmail.com>
2022-05-04 15:01:49 -0400

    Initial commit in terraform tryout project.

---
+++ main.tf
@@ -0,0 +1,31 @@
+# This element I think sets various configuration options for terraform itself.
+terraform {
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+            version = ">= 2.13.0"
+        }
+    }
+}
+
+provider "docker" {
+    host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+    name  = "nginx:latest"
+    keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.latest
+    name = "tutorial"
+    ports {
+        internal = 80
+        external = 8000
+    }
+}
+
+
+
+
